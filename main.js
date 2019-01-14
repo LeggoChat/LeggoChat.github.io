fbc7e379ab0128a1d6508b4478ceeeaaca903eee
Desmond <cc.chin@techninier.com>
2019-01-14 16:59:33 +0800

    -Added message persistence -Added disconnection detection -Enhanced UX and UI

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var currentChn = 'general';
+var currentPage = 0;
 var linkChn = window.location.hash.substr(1);
 if (linkChn !== '') {
     currentChn = linkChn;
@@ -15,18 +16,33 @@ $('#connecting').modal('show');
 
 var socket = io.connect('https://leggochat.hexabyn.com');
 socket.on('status', function (data) {
-    log(data);
+    document.getElementById('message').disabled = false;
+    document.getElementById("sendBtn").disabled = false;
+    if(currentPage > 0){
+        document.getElementById('log').innerHTML = "";
+        currentPage = 0;
+        log({ from: "notice", message: "Reconnected Successfully." });
+    }else{
+        log(data);
+    }
     $('#connecting').modal('hide');
     socket.emit('client', { channel: currentChn });
+    socket.emit('messages', { channel: currentChn, page: currentPage });
 });
 
 socket.on(currentChn, function (data) {
-    console.log(data);
     log(data);
 });
 
 socket.on('disconnect', function (data) {
-    log(data);
+    //log(data);
+    log({ from: "notice", message: "Disconnected!" });
+    document.getElementById('message').disabled = true;
+    document.getElementById("sendBtn").disabled = true;
+});
+
+socket.on('messages', function (data) {
+    prelog(data);
 });
 
 function send() {
@@ -34,7 +50,7 @@ function send() {
         document.getElementById('message').value = "";
         return false;
     }
-    socket.emit('trigger', { channel: currentChn, message: document.getElementById('message').value });
+    socket.emit('trigger', { channel: currentChn, persist: document.getElementById('persist').checked, message: document.getElementById('message').value });
     // log("Me: " + document.getElementById('message').value);
     log({from: "Me", message: document.getElementById('message').value});
     document.getElementById('message').value = "";
@@ -47,6 +63,54 @@ function join() {
     setTimeout(function(){
         location.reload();
     },1500)
+}
+
+function prelog(data){
+    currentPage++;
+    // data = JSON.parse(data);
+    data = JSON.parse(data.message);
+    var html = "";
+    html += `<div id="load-more" class="container text-center"><button onclick="loadMessages();" class="btn btn-link">Load More</button></div>`;
+
+    for(var i=data.length-1; i>=0; i--){
+        html += `
+                <div class="container">
+                    <div class="row">
+                        <div class="card bg-danger col-12 col-sm-6">
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <span>${data[i].message}</span>
+                                </h4>
+                                <div class="card-stats">
+                                    <div class="author">
+                                        <span>${data[i].name}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 d-none d-sm-block">
+                            &nbsp;
+                        </div>
+                    </div>
+                </div>
+            `;
+    }
+    if(currentPage > 1){
+        var elem = document.querySelector('#load-more');
+        elem.parentNode.removeChild(elem);
+    }
+    document.getElementById('log').innerHTML = html+document.getElementById('log').innerHTML;
+
+    if(currentPage == 1){
+        var objDiv = document.getElementsByTagName("BODY")[0];
+        objDiv.scrollTop = objDiv.scrollHeight;
+
+        window.scrollTo(0,document.body.scrollHeight);
+    }
+}
+
+function loadMessages(){
+    socket.emit('messages', { channel: currentChn, page: currentPage });
 }
 
 function log(msg) {
@@ -64,7 +128,11 @@ function log(msg) {
     }else if(msg.from == "count"){
         document.getElementById('count').innerHTML = msg.message;
     }else{
+        var persistStr = ""
         if(msg.from == "Me"){
+            if(document.getElementById('persist').checked){
+                persistStr = " - Persisted";
+            }
             var html = `
                 <div class="container"><div class="row">
                     <div class="col-sm-6 d-none d-sm-block">
@@ -78,13 +146,16 @@ function log(msg) {
 
                             <div class="card-stats">
                                 <div class="author">
-                                    <span>${msg.from}</span>
+                                    <span>${msg.from} ${persistStr}</span>
                                 </div>
                             </div>
                     </div>
                 </div></div>
             `;
         }else{
+            if(msg.persist == 1 || msg.persist == true || msg.persist == "true"){
+                persistStr = " - Persisted";
+            }
             var html = `
                 <div class="container"><div class="row">
                     <div class="card bg-info col-12 col-sm-6">
@@ -95,7 +166,7 @@ function log(msg) {
 
                             <div class="card-stats">
                                 <div class="author">
-                                    <span>${msg.from}</span>
+                                    <span>${msg.from} ${persistStr}</span>
                                 </div>
                             </div>
                     </div>
