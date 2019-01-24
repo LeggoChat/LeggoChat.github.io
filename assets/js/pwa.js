a59794c728f563320d11a973dc314110a278675a
Desmond <cc.chin@techninier.com>
2019-01-24 22:16:19 +0800

    added scope for service worker

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -37,8 +37,9 @@ btnAdd.addEventListener('click', (e) => {
 
 if ('serviceWorker' in navigator) {
     console.log("Will the service worker register?");
-    navigator.serviceWorker.register('service-worker.js')
-        .then(function (reg) {
+    navigator.serviceWorker.register('service-worker.js', {
+        scope: '.' // <--- THIS BIT IS REQUIRED
+    }).then(function (reg) {
             console.log("Yes, it did.");
         }).catch(function (err) {
             console.log("No it didn't. This happened: ", err)
